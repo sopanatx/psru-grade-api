5c7c75ab64dfbb089b3954f3ba202407645e4aef
Sopanat <sopanus2014@gmail.com>
2020-08-19 22:08:49 +0700

    Add Activity info and fix listen bugs.

--- app.js
+++ app.js
@@ -1,40 +1,49 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 var app = express();
-const serverless = require('serverless-http');
+const serverless = require("serverless-http");
+var http = require("http");
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
+});
+
+app.set("port", process.env.PORT || 8000); //<--- replace with your port number
+
+// Server
+var server = http.createServer(app);
+server.listen(app.get("port"), function () {
+  console.log("Express server listening on port " + app.get("port"));
 });
 
 module.exports.handler = serverless(app);
--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const getGrade = require('../service/getGrade')
-const axios = require('axios');
+const getGrade = require("../service/getGrade");
+const axios = require("axios");
 const queryString = require("query-string");
-const iconv  = require('iconv-lite');
+const iconv = require("iconv-lite");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-
-
-router.get('/grade/:id',function(req,res){
-console.log(req.params)
-const requestBody = {
+router.get("/grade/:id", function (req, res) {
+  console.log(req.params);
+  const requestBody = {
     ID_NO: req.params.id,
   };
-const config = {
+  const config = {
     header: {
       Origin: "https://api.itpsru.in.th/",
       "Content-Type": "application/x-www-form-urlencoded",
@@ -25,16 +23,43 @@ const config = {
       "Accept-Encoding": "gzip, deflate",
       "Accept-Language": "th-GB,th;q=0.9,en-GB;q=0.8,en;q=0.7,th-TH;q=0.6",
     },
-    responseType: 'arraybuffer',
-     responseEncoding: 'binary'
+    responseType: "arraybuffer",
+    responseEncoding: "binary",
   };
 
+  const studentGrade = axios
+    .post(
+      "http://202.29.80.113/cgi/LstGrade1.pl",
+      queryString.stringify(requestBody),
+      config
+    )
+    .then((result) => {
+      res.status(200).send(iconv.decode(new Buffer(result.data), "TIS-620"));
+    });
+});
 
- const studentGrade = axios
-    .post("http://202.29.80.113/cgi/LstGrade1.pl", queryString.stringify(requestBody), config).then((result) => {
-     res.status(200).send(iconv.decode(new Buffer(result.data),"TIS-620"))
-})
-    })
-      
-
+router.get("/activity/:id", function (req, res) {
+  const requestBody = {
+    search_val: req.params.id,
+  };
+  const config = {
+    header: {
+      Origin: "https://api.itpsru.in.th/",
+      "Content-Type": "application/x-www-form-urlencoded",
+      Referer: "https://api.itpsru.in.th/",
+      "Accept-Encoding": "gzip, deflate",
+      "Accept-Language": "th-GB,th;q=0.9,en-GB;q=0.8,en;q=0.7,th-TH;q=0.6",
+    },
+  };
+  axios
+    .post(
+      "http://202.29.80.144/activity/search_detail.php",
+      queryString.stringify(requestBody),
+      config
+    )
+    .then((result) => {
+      res.status(200).send(result.data);
+    });
+});
+//
 module.exports = router;
