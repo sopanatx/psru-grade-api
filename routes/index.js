e7460267862904696c74ef2fe946ce33bd446c8c
sopanatx <sopanus2014@gmail.com>
2020-12-26 12:16:57 +0700

    feat : Initial Get Billing API

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const iconv = require("iconv-lite");
 const cheerio = require("cheerio");
 const HtmlTableToJson = require("html-table-to-json");
 const { json } = require("express");
-const { Unauthorized, Forbidden } = require("http-errors");
+const { Unauthorized, Forbidden, MethodNotAllowed } = require("http-errors");
 const { checkIsAssess } = require("../service/Grade");
 const { convertDayPeriodType } = require("../utils/misc");
 /* GET home page. */
@@ -915,5 +915,105 @@ router.get("/class/:id", async function (req, res) {
         "ระบบ API ไม่สามารถเชื่อมต่อเครื่องเซิร์ฟเวอร์แม่ข่าย / (REG_PSRU_WEBSITE) ได้.",
     });
   }
+});
+
+router.get("/api/billing", async (req, res) => {
+  res.send(new MethodNotAllowed());
+});
+
+router.post("/api/billing", async (req, res) => {
+  const requestBody = {
+    std_id: req.body.studentId,
+    std_password: req.body.studentPassword,
+  };
+  const getCookie = await axios.post(
+    "https://reg3.psru.ac.th/regist1/CheckLogin1_n1.php",
+
+    queryString.stringify(requestBody),
+    {
+      timeout: 8000,
+    }
+  );
+
+  let cookie;
+  let session;
+  cookie = getCookie.headers["set-cookie"];
+  cookie = cookie[0].split(" ", 1)[0];
+
+  const getStudentInfo = await axios.get(
+    "https://reg3.psru.ac.th/regist1/tsc1_n1_load.php",
+
+    {
+      headers: {
+        Cookie: ` datas_1=; datas_2=; datas_3=; datas_4=; datas_5=; datas_6=; datas_7=; datas_8=; datas_9=; ${cookie}`,
+      },
+      timeout: 8000,
+      responseType: "arraybuffer",
+      responseEncoding: "binary",
+    }
+  );
+  //console.log(getStudentInfo.headers);
+  const $ = cheerio.load(
+    iconv.decode(new Buffer(getStudentInfo.data), "windows-874")
+  );
+
+  let studentBillingInfo = {
+    studentId: $("body > font > b:nth-child(2)").text(),
+    studentName: $("body > font > b:nth-child(3)").text(),
+    studentCourse: $("body > font > u:nth-child(5)").text(),
+    studentMajor: $("body > font > u:nth-child(5)").text(),
+    studentGroup: $("body > font > u:nth-child(7)").text(),
+    studentType: $("body > font > u:nth-child(9)").text(),
+    isRegistered: $("body > font > font").text() ? true : false,
+  };
+
+  const uniqueUrl = $("body > font > a:nth-child(16)").attr("href");
+
+  console.log(uniqueUrl);
+  const getBillingInfo = await axios.get(
+    `https://reg3.psru.ac.th/${uniqueUrl}`,
+
+    {
+      headers: {
+        Cookie: ` datas_1=; datas_2=; datas_3=; datas_4=; datas_5=; datas_6=; datas_7=; datas_8=; datas_9=; ${cookie}`,
+      },
+      timeout: 8000,
+      responseType: "arraybuffer",
+      responseEncoding: "binary",
+    }
+  );
+
+  const $2 = cheerio.load(
+    iconv.decode(new Buffer(getBillingInfo.data), "windows-874")
+  );
+  console.log(
+    $2(
+      "body > center > font > table > tbody > tr:nth-child(3) > td > table:nth-child(7) > tbody > tr > td > p:nth-child(2) > font > font > font:nth-child(5) > b:nth-child(1) > u"
+    ).html()
+  );
+  let studentPaymentInfo = {
+    serviceCode: $2(
+      "body > center > font > table > tbody > tr:nth-child(3) > td > table:nth-child(7) > tbody > tr > td > p:nth-child(2) > font > font > font:nth-child(3) > b > u"
+    ).text(),
+    companyName: $2(
+      "body > center > font > table > tbody > tr:nth-child(3) > td > table:nth-child(7) > tbody > tr > td > p:nth-child(2) > font > font > font:nth-child(4) > b:nth-child(1) > u"
+    ).text(),
+    customer: $2(
+      "body > center > font > table > tbody > tr:nth-child(3) > td > table:nth-child(7) > tbody > tr > td > p:nth-child(2) > font > font > font:nth-child(4) > b:nth-child(2) > u"
+    ).text(),
+    ref1: $2(
+      "body > center > font > table > tbody > tr:nth-child(3) > td > table:nth-child(7) > tbody > tr > td > p:nth-child(2) > font > font > font:nth-child(5) > b:nth-child(1) > u"
+    ).text(),
+    ref2: $2(
+      "body > center > font > table > tbody > tr:nth-child(3) > td > table:nth-child(7) > tbody > tr > td > p:nth-child(2) > font > font > font:nth-child(5) > b:nth-child(2) > u"
+    ).text(),
+    totalAmount: $2(
+      "#table3 > tbody > tr:nth-child(4) > td:nth-child(2) > font"
+    ).text(),
+    paymentUntil: $2(
+      "body > center > font > table > tbody > tr:nth-child(3) > td > table:nth-child(7) > tbody > tr > td > p:nth-child(2) > font > font > font:nth-child(7)"
+    ).text(),
+  };
+  res.send([studentBillingInfo, studentPaymentInfo]);
 });
 module.exports = router;
