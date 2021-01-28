41611d631db410f421d5d213a52fc6aa54b4e851
Sopanat Suradach <sopanus2014@gmail.com>
2021-01-28 14:14:38 +0700

    feat : request grade with semester.

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ const { json } = require("express");
 const { Unauthorized, Forbidden, MethodNotAllowed } = require("http-errors");
 const { checkIsAssess } = require("../service/Grade");
 const { convertDayPeriodType } = require("../utils/misc");
+const { body, validationResult } = require("express-validator");
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.send(new Forbidden());
@@ -431,110 +432,134 @@ router.get("/api/activity/:id", async (req, res) => {
     });
 });
 
-router.get("/api/grade/:id", async function (req, res) {
-  if (req.params.id.length < 9) {
-    res.status(400).send(new Unauthorized("StudentID Length Must be equal 10"));
-  } else if (req.params.id.length > 10) {
-    res.status(400).send(new Unauthorized("StudentID Length Must be equal 10"));
+router.post(
+  "/api/grade/",
+  body("studentId").isLength({ min: 10, max: 10 }),
+  body("semester").isString().isLength({ min: 6, max: 10 }),
+  async function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { studentId, semester, apiKey } = req.body;
+    if (!studentId || studentId.length < 9) {
+      res
+        .status(400)
+        .send(new Unauthorized("StudentID Length Must be equal 10"));
+    } else if (studentId.length > 10) {
+      res
+        .status(400)
+        .send(new Unauthorized("StudentID Length Must be equal 10"));
+    } else {
+      const requestBody = {
+        ID_NO: studentId,
+      };
+
+      const config = {
+        header: {
+          Origin: "https://api.itpsru.in.th/",
+          "Content-Type": "application/x-www-form-urlencoded",
+          Referer: "https://api.itpsru.in.th/",
+          "Accept-Encoding": "gzip, deflate",
+          "Accept-Language": "th-GB,th;q=0.9,en-GB;q=0.8,en;q=0.7,th-TH;q=0.6",
+        },
+        responseType: "arraybuffer",
+        responseEncoding: "binary",
+      };
+      if (!(await checkIsAssess(req.params.id))) {
+        res.status(403).json({
+          errorCode: 1001,
+          errorMessage: "API_GRADE_ASSESS_ERROR",
+          th:
+            "ไม่สามารถแสดงผลการเรียนได้ เนื่องจากท่านประเมินการสอนออนไลน์ยังไม่ครบทุกรายวิชาในเทอมนี้.",
+        });
+      } else {
+        const studentGrade = axios
+          .post(
+            "http://202.29.80.113/cgi/LstGrade1.pl",
+            queryString.stringify(requestBody),
+            config
+          )
+          .then((result) => {
+            const html = iconv.decode(new Buffer.from(result.data), "TIS-620");
+            const $ = cheerio.load(html);
+            const scrappedTable = [];
+            const gradeTable = $(
+              "body > center > table > tbody > tr > td > font > center:nth-child(2) > table > tbody > tr > td > table > tbody > tr > td"
+            ).each((index, element) => {
+              scrappedTable.push($(element).text());
+            });
+            //   console.log(scrappedTable);
+            const studentInfo = [];
+            $(
+              "body > center > table > tbody > tr > td > font > center:nth-child(2) > table > tbody > tr > td > font:nth-child(1) > center"
+            ).each((index, element) => {
+              let parseinfo = $(element).text().split(" ", 10);
+              studentInfo.push(
+                Object.assign({
+                  studentId: parseinfo[1],
+                  studentFirstName: parseinfo[4],
+                  studentLastName: parseinfo[6],
+                  graduatedFrom: parseinfo[8],
+                })
+              );
+            });
+            const groupGrade = [];
+
+            for (let i = 1; i < scrappedTable.length / 7; i++) {
+              groupGrade.push(scrappedTable.slice(i * 7, i * 7 + 7));
+            }
+            let TotalCalculateGrade = [];
+            const TotalCalculateScrapped = $(
+              "body > center > table > tbody > tr > td > font > center:nth-child(2) > table > tbody > tr > td > font:nth-child(3)"
+            ).each((index, element) => {
+              const convertTotalCalculate = $(element).text().split(" ", 10);
+              //  console.log(convertTotalCalculate);
+              TotalCalculateGrade.push(
+                Object.assign({
+                  TotalCredit: Number(convertTotalCalculate[1]),
+                  TotalAverageGrade: Number(convertTotalCalculate[5]),
+                  TotalMainSubjectGrade: Number(
+                    convertTotalCalculate[8].substr(0, 4)
+                  ),
+                })
+              );
+            });
+            let StudentGrade = [];
+            let availableSemesterData = [];
+            let currentSemester = semester;
+            for (let j = 0; j < groupGrade.length; j++) {
+              if (availableSemesterData.indexOf(`${groupGrade[j][0]}`) != -1) {
+              } else {
+                availableSemesterData.push(groupGrade[j][0]);
+              }
+              if (groupGrade[j][0] == `${semester}`) {
+                StudentGrade.push(
+                  Object.assign({
+                    section: groupGrade[j][1],
+                    subjectCode: groupGrade[j][2],
+                    subjectName: groupGrade[j][3],
+                    credit: groupGrade[j][4],
+                    studentGrade: groupGrade[j][5],
+                    subjectGroup: groupGrade[j][6],
+                  })
+                );
+              } else {
+              }
+            }
+
+            res.send({
+              studentInfo,
+              TotalCalculateGrade,
+              availableSemesterData,
+
+              currentSemester: [semester, StudentGrade],
+            });
+          });
+      }
+    }
   }
-  console.log(req.params);
-  const requestBody = {
-    ID_NO: req.params.id,
-  };
-
-  const config = {
-    header: {
-      Origin: "https://api.itpsru.in.th/",
-      "Content-Type": "application/x-www-form-urlencoded",
-      Referer: "https://api.itpsru.in.th/",
-      "Accept-Encoding": "gzip, deflate",
-      "Accept-Language": "th-GB,th;q=0.9,en-GB;q=0.8,en;q=0.7,th-TH;q=0.6",
-    },
-    responseType: "arraybuffer",
-    responseEncoding: "binary",
-  };
-  if (!(await checkIsAssess(req.params.id))) {
-    res.status(403).json({
-      errorCode: 1001,
-      errorMessage: "API_GRADE_ASSESS_ERROR",
-      th:
-        "ไม่สามารถแสดงผลการเรียนได้ เนื่องจากท่านประเมินการสอนออนไลน์ยังไม่ครบทุกรายวิชาในเทอมนี้.",
-    });
-  } else {
-    const studentGrade = axios
-      .post(
-        "http://202.29.80.113/cgi/LstGrade1.pl",
-        queryString.stringify(requestBody),
-        config
-      )
-      .then((result) => {
-        const html = iconv.decode(new Buffer.from(result.data), "TIS-620");
-        const $ = cheerio.load(html);
-        const scrappedTable = [];
-        const gradeTable = $(
-          "body > center > table > tbody > tr > td > font > center:nth-child(2) > table > tbody > tr > td > table > tbody > tr > td"
-        ).each((index, element) => {
-          scrappedTable.push($(element).text());
-        });
-        console.log(scrappedTable);
-        const studentInfo = [];
-        $(
-          "body > center > table > tbody > tr > td > font > center:nth-child(2) > table > tbody > tr > td > font:nth-child(1) > center"
-        ).each((index, element) => {
-          let parseinfo = $(element).text().split(" ", 10);
-          studentInfo.push(
-            Object.assign({
-              studentId: parseinfo[1],
-              studentFirstName: parseinfo[4],
-              studentLastName: parseinfo[6],
-              graduatedFrom: parseinfo[8],
-            })
-          );
-        });
-        const groupGrade = [];
-
-        for (let i = 1; i < scrappedTable.length / 7; i++) {
-          groupGrade.push(scrappedTable.slice(i * 7, i * 7 + 7));
-        }
-        let TotalCalculateGrade = [];
-        const TotalCalculateScrapped = $(
-          "body > center > table > tbody > tr > td > font > center:nth-child(2) > table > tbody > tr > td > font:nth-child(3)"
-        ).each((index, element) => {
-          const convertTotalCalculate = $(element).text().split(" ", 10);
-          console.log(convertTotalCalculate);
-          TotalCalculateGrade.push(
-            Object.assign({
-              TotalCredit: Number(convertTotalCalculate[1]),
-              TotalAverageGrade: Number(convertTotalCalculate[5]),
-              TotalMainSubjectGrade: Number(
-                convertTotalCalculate[8].substr(0, 4)
-              ),
-            })
-          );
-        });
-        let StudentGrade = [];
-        for (let j = 0; j < groupGrade.length; j++) {
-          StudentGrade.push(
-            Object.assign({
-              term: groupGrade[j][0],
-              section: groupGrade[j][1],
-              subjectCode: groupGrade[j][2],
-              subjectName: groupGrade[j][3],
-              credit: groupGrade[j][4],
-              studentGrade: groupGrade[j][5],
-              subjectGroup: groupGrade[j][6],
-            })
-          );
-        }
-
-        res.send({
-          studentInfo,
-          TotalCalculateGrade,
-          StudentGrade,
-        });
-      });
-  }
-});
+);
 
 router.get("/api/class/:id", async function (req, res) {
   async function removeElement(array, elem) {
